6c52cc5688a1d0496a44fa8702ffce6a82d19f6b
Clavain <clavainova@gmail.com>
2019-12-12 12:08:25 +0100

    startng bomb placement

--- 4.3_POO/js/BombeList.js
+++ 4.3_POO/js/BombeList.js
@@ -3,13 +3,21 @@ import { Object } from "./Object.js";
 //props inherited from object:      url, width, height, img
 //methods inherited from object:    draw(), despawn()
 
-export class BombList{
+export class BombList {
     constructor(bombs) {
         this.bombs = bombs;
     }
 
     push(bomb) {
         this.bombs.push(bomb);
+    }
+
+    positionBombs(maxX, maxY, ctx) {
+        this.bombs.forEach(bomb => {
+            let x = randomNum(maxX, 0);
+            let y = randomNum(maxY, 0);
+            item.draw(ctx, x, y)
+        });
     }
 }
 
