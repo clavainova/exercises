4519d953855cc04116cc8c49909e20979b419527
Clavain <clavainova@gmail.com>
2019-12-17 16:57:50 +0100

    promesses progress

--- 4.6_Promesses/2_VasYFonce/main.js
+++ 4.6_Promesses/2_VasYFonce/main.js
@@ -1,9 +1,31 @@
 import { Auteur } from "./auteur.js";
 "use strict";
 
+var url = "anatole.json";
+var anatArr = JSONget();
 
-function getJson(){
-    $.getJSON("1_fromages.json", function (data) {
+url = "promesses.json";
+var promArr = JSONget();
 
+function JSONget() {
+    return new Promise(function (resolve, reject) {
+        let xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                let arr = JSON.parse(this.responseText);
+                resolve(arr);
+            }
+        }
     });
 }
+
+(async () => {
+    try {
+        await JSONget();
+        console.log(anatArr);
+    } catch (e) {
+        console.log(e);
+    }
+})();
