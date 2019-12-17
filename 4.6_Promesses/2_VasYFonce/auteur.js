eb3a697a8849d9a814654e079ac85a8f04109179
Clavain <clavainova@gmail.com>
2019-12-17 14:46:58 +0100

    vas y fonce

---
+++ 4.6_Promesses/2_VasYFonce/auteur.js
@@ -0,0 +1,18 @@
+export class Auteur {
+    "use strict";
+    constructor(nom, promesse, jsonUrl, description, wikiUrl) {
+        this.nom = nom;
+        this.promesse = promesse;
+        this.jsonUrl = jsonUrl;
+        this.description = description;
+        this.wikiUrl = wikiUrl;
+    }
+
+    getProp(index) {
+        return this[index];
+    }
+
+    setProp(index, value) {
+        this[index] = value;
+    }
+}
---
+++ 4.6_Promesses/2_VasYFonce/main.js
@@ -0,0 +1,9 @@
+import { Auteur } from "./auteur.js";
+"use strict";
+
+
+function getJson(){
+    $.getJSON("1_fromages.json", function (data) {
+
+    });
+}
