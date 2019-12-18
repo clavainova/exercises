e404c324f5d24547f276c193689d732fe8617a37
Clavain <clavainova@gmail.com>
2019-12-18 10:11:06 +0100

    promesses fini

--- 4.6_Promesses/2_VasYFonce/main.js
+++ 4.6_Promesses/2_VasYFonce/main.js
@@ -1,13 +1,47 @@
 import { Auteur } from "./auteur.js";
 "use strict";
 
-var url = "anatole.json";
-var anatArr = JSONget();
+let nom;
+let promesse;
+let jsonUrl;
+let description;
+let wikiUrl;
 
-url = "promesses.json";
-var promArr = JSONget();
+var people = [];
 
-function JSONget() {
+makePerson("promesses.json")
+    // .then(() => {
+    //     people.forEach(element =>
+    //         console.log("element: " + element))
+    // });
+
+
+function makePerson(url1) {
+    //get the personal info first
+    let promArr = JSONget(url1);
+    promArr.then(function (value) {
+        nom = value.auteur;
+        promesse = value.promesse;
+        jsonUrl = value.url;
+        //then get the other info
+    }).then(function () {
+        let anatArr = JSONget(jsonUrl);
+        anatArr.then(function (value) {
+            description = value.description;
+            wikiUrl = value.url;
+            //once we have all the info, we make it an object
+        }).then(function () {
+            let anatole = new Auteur(nom, promesse, jsonUrl, description, wikiUrl);
+            //now we push the object onto the array
+            people.push(anatole);
+            console.log("updating array:");
+            console.log(people);
+        });
+    });
+}
+
+
+function JSONget(url) {
     return new Promise(function (resolve, reject) {
         let xmlhttp = new XMLHttpRequest();
         xmlhttp.open("GET", url, true);
@@ -15,17 +49,19 @@ function JSONget() {
         xmlhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
                 let arr = JSON.parse(this.responseText);
+                //resolves when recieves json, returns data
                 resolve(arr);
             }
         }
     });
 }
 
-(async () => {
-    try {
-        await JSONget();
-        console.log(anatArr);
-    } catch (e) {
-        console.log(e);
-    }
-})();
+// (async () => {
+//     try {
+//         await JSONget();
+//         console.log(anatArr);
+//     } catch (e) {
+//         console.log("caught");
+//         console.log(e);
+//     }
+// })();
