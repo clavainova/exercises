bec5ba32772db8be219098401c369d48f64389d4
Clavain <clavainova@gmail.com>
2020-01-17 11:34:11 +0100

    ppj progress

--- 5.2_DRY_PHP/ppj/contact.php
+++ 5.2_DRY_PHP/ppj/contact.php
@@ -11,102 +11,16 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <style>
-        body {
-            font: 20px Montserrat, sans-serif;
-            line-height: 1.8;
-            color: #f5f6f7;
-        }
-
-        p {
-            font-size: 16px;
-        }
-
-        .margin {
-            margin-bottom: 45px;
-        }
-
-        .bg-1 {
-            background-color: #1abc9c;
-            /* Green */
-            color: #ffffff;
-        }
-
-        .bg-2 {
-            background-color: #474e5d;
-            /* Dark Blue */
-            color: #ffffff;
-        }
-
-        .bg-3 {
-            background-color: #ffffff;
-            /* White */
-            color: #555555;
-        }
-
-        .bg-4 {
-            background-color: #2f2f2f;
-            /* Black Gray */
-            color: #fff;
-        }
-
-        .container-fluid {
-            padding-top: 70px;
-            padding-bottom: 70px;
-        }
-
-        .navbar {
-            padding-top: 15px;
-            padding-bottom: 15px;
-            border: 0;
-            border-radius: 0;
-            margin-bottom: 0;
-            font-size: 12px;
-            letter-spacing: 5px;
-        }
-
-        .navbar-nav li a:hover {
-            color: #1abc9c !important;
-        }
-        #logo{
-            padding : 0;
-        }
+        <?php include 'components/style.css'
+        ?>
     </style>
 </head>
 
 <body>
 
-    <!-- Navbar -->
-    <nav class="navbar navbar-default">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a id="logo" class="navbar-brand" href="index.html"><img src="images/bird.jpg"  alt="logo" width="50px;"></a>
-            </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="index.html">ACCUEIL</a>
-                    </li>
-                    <li>
-                        <a href="produits.html">NOS PRODUITS</a>
-                    </li>
-                    <li>
-                        <a href="blog.html">BLOG</a>
-                    </li>
-                    <li>
-                        <a href="contact.html">CONTACT</a>
-                    </li>
-                    <li>
-                        <a href="about.html">ABOUT</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php
+    include 'components/nav.html';
+    ?>
 
     <!-- First Container -->
     <div class="container-fluid bg-1">
@@ -132,12 +46,9 @@
         </form>
     </div>
 
-    <!-- Footer -->
-    <footer class="container-fluid bg-4 text-center">
-        <p>Bootstrap Theme Made By
-            <a href="https://www.w3schools.com">www.w3schools.com</a>
-        </p>
-    </footer>
+    <?php
+    include 'components/footer.html';
+    ?>
 
 </body>
 
